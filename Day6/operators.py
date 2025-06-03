99b1a4a760a5ab93d011993a56e1b7c1465eb183
GauravDevops15 <gauravatwork15@gmail.com>
2025-06-03 14:46:29 +0000

    Identity operators

--- Day6/operators.py
+++ Day6/operators.py
@@ -7,3 +7,20 @@ print(A)
 
 B=3/2
 print(B)
+
+#Identity operator
+
+a=5
+b=5
+
+print(a is b)
+
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+
+print(a is b)      # True (same object in memory)
+print(a is c)      # False (same values, different objects)
+print(a == c)      # True (values are equal)
+print(a is not c)  # True (different objects)
+

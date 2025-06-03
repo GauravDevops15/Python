c79a781ce6671432062dc6adc5e1c11004a06bfa
GauravDevops15 <gauravatwork15@gmail.com>
2025-06-03 15:10:06 +0000

    details of list n tuple

--- Day8/Lists&Tuples.py
+++ Day8/Lists&Tuples.py
@@ -2,3 +2,19 @@
 
 students_names = ["Abhi","Gaurav","Mansi"]
 print(students_names)
+
+## Helps in printing s3 buckets, Ec2 instances
+
+#Tuple
+
+###tuples are immutable,means can't be resized
+
+admins = ("Abhi","Gaurav")
+print(admins)
+
+S3_bucket_lists = ("abhi_demo_bucket","ramu_demo_bucket")
+print(S3_bucket_lists)
+
+print(type(S3_bucket_lists))#check type
+print(type(admins))#check type
+print(type(students_names))#check type

0a9e7a6fe484d87324637ded2d7fff52d84e1624
GauravDevops15 <gauravatwork15@gmail.com>
2025-06-08 08:23:44 +0000

    For & while Loops

---
+++ Day9/Loops.py
@@ -0,0 +1,14 @@
+#Loops in python For and While loop
+##for loop when iteration is ddetermined
+##While loop when iteration is not specified
+
+#for i in range (10) :
+#    print (i)
+
+
+colors = ['yellow','pink','red','orange','black','white']
+
+for color in colors :
+    print (color)
+
+
